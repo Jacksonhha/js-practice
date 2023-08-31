9d6254d7139248fb5b9d60fda992e4cc48dbab8b
徐敬傑 <takesi@mitake.com.tw>
2023-08-31 14:42:29 +0800

    成功

--- src/week1/test/sum.js
+++ src/week1/test/sum.js
@@ -9,7 +9,7 @@
  * 返回 5
  * sum(2,3)
  *
- * @example
+ * @example  //先轉成字串整數再換數字
  * 返回 0.6
  * sum(0.2,0.4)
  *
@@ -18,9 +18,22 @@
  *
  * @return {number} 兩個數的和
  */
-function sum(numA, numB) {
+function num(numA, numB, precision = 12) {
+  //precision 這個參數允許你指定一個數值，用來控制計算結果的小數位數（精準度）
+
   // TODO: 完成這個 function
-  return numA + numB;
+  // 目前只解決的整數的問題，剩小數點
+  //   const AA = numA * 10;
+  //   const BB = numB * 10;
+
+  //   return (AA + BB) / 10;
+
+  const all = parseFloat(numA) + parseFloat(numB);
+  // parseFloat(函式會試圖解析輸入的字符串，並提取其中的數值部分，然後返回對應的浮點數。)
+
+  return +all.toPrecision(precision);
+  //+是將浮點數轉換成數值 .toPrecision(計算機表示上進行四捨五入，放置precision更能抓到精準度)
+  //https://github.com/camsong/blog/issues/9  資料來源
 }
 
-export default sum;
+export default num;
